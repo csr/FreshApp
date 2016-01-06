b6fd498a3e0635f44c523c612a14ef2b3cc1b6fc
Cesare de Cal <hi@cesare.io>
2016-01-06 09:45:20 +0100

    Heavy clean up  - removed sign in and sign up
    We decided to redefine the goals of Fresh and to keep it as simple as
    possible.

--- Fresh/Fresh/MapViewController.swift
+++ Fresh/Fresh/MapViewController.swift
@@ -15,23 +15,14 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
     @IBOutlet weak var getLocationView: UIView!
     @IBOutlet weak var getLocationButton: UIButton!
     @IBOutlet weak var profileNavigationBarButton: UIBarButtonItem!
-
-    var smallCustomPin = SmallPin()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setUpLocationManager()
         updateUserLocation()
-
         addSearchBarToNavigationBar()
-        smallCustomPin = SmallPin.loadNib()
-        checkIfObjectsHaveCoordinates()
         addCustomPinsToMap()
-        
-        if PFUser.currentUser() == nil {
-            askLogInOrSignUp()
-        }
     }
     
     func setUpLocationManager() {
@@ -47,41 +38,10 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
         locationManager.startUpdatingLocation()
     }
     
-    func checkIfObjectsHaveCoordinates() {
-        PFQuery(className: "Products").findObjectsInBackgroundWithBlock {
-            (objects: [PFObject]?, error: NSError?) in
-            if objects != nil {
-                for object in objects! {
-                    if object["Latitude"] == nil || object["Longitude"] == nil {
-                        self.convertObjectLocationToCoordinates(object)
-                    }
-                }
-            } else if error != nil {
-                print(error)
-            }
-        }
-    }
-    
     func addSearchBarToNavigationBar() {
         let searchBar = UISearchBar()
         searchBar.placeholder = "Search for local markets, farmers or products..."
         self.navigationItem.titleView = searchBar
-    }
-    
-    func convertObjectLocationToCoordinates(myObject: PFObject) {
-        let geocoder = CLGeocoder()
-        geocoder.geocodeAddressString(myObject["Location"] as! String, completionHandler: { (placemarks, error) -> Void in
-            if let placemark = placemarks?[0] as CLPlacemark! {
-                myObject["Latitude"] = Float(placemark.location!.coordinate.latitude)
-                myObject["Longitude"] = Float(placemark.location!.coordinate.longitude)
-                myObject.saveInBackgroundWithBlock {
-                    (success: Bool, error: NSError?) -> Void in
-                    if (error != nil) {
-                        print(error)
-                    }
-                }
-            }
-        })
     }
     
     @IBAction func changeStateImageOfGetLocationButton(sender: AnyObject) {
@@ -135,177 +95,6 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
                 print(error)
                 myCustomPin.coordinate = CLLocationCoordinate2DMake(0, 0)
             }
-        }
-    }
-    
-    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
-        if annotation is MKUserLocation {
-            return mapView.dequeueReusableAnnotationViewWithIdentifier("")
-        } else {
-            let annotationView = MKAnnotationView(frame: CGRectMake(0, 0, 100, 45))
-            let query = PFQuery(className:"Products")
-            query.fromLocalDatastore()
-            query.findObjectsInBackgroundWithBlock {
-                (objects: [PFObject]?, error: NSError?) -> Void in
-                print("Successfully retrieved \(objects!.count) objects in the local datastore in ViewForAnnotation()")
-                if objects != nil {
-                    for object in objects! {
-                        self.smallCustomPin.labelTitle.text = object["Title"] as? String
-                    }
-                } else if error != nil {
-                    print(error)
-                }
-            }
-            
-            annotationView.centerOffset = CGPointMake(0, -50)
-            annotationView.backgroundColor = UIColor.whiteColor()
-            annotationView.layer.cornerRadius = 6
-            annotationView.addSubview(smallCustomPin)
-            annotationView.enabled = true
-            annotationView.canShowCallout = false
-            annotationView.annotation = annotation
-            return annotationView
-        }
-    }
-    
-    func tapOnSmallCustomPin() {
-        print("Tap on small custom pin!")
-    }
-    
-    @IBAction func showProfileSettings (sender: UIBarButtonItem) {
-        let profileOptions = UIAlertController()
-        let currentUser = PFUser.currentUser()
-        
-        if (currentUser == nil) {
-            profileOptions.addAction(UIAlertAction(title: "Sign up", style: UIAlertActionStyle.Default, handler: {(action: UIAlertAction!) -> Void in
-                self.signUp()
-            }))
-            
-            profileOptions.addAction(UIAlertAction(title: "Log in", style: UIAlertActionStyle.Default, handler: { (action: UIAlertAction!) -> Void in
-                self.signIn()
-            }))
-        } else {
-            profileOptions.addAction(UIAlertAction(title: "Sign out", style: UIAlertActionStyle.Default, handler: { (action: UIAlertAction!) -> Void in
-                PFUser.logOut()
-            }))
-        }
-        profileOptions.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Destructive, handler: nil))
-        
-        // Display the action sheet
-        profileOptions.popoverPresentationController?.barButtonItem = profileNavigationBarButton
-        presentViewController(profileOptions, animated: true, completion: nil)
-    }
-    
-    func askLogInOrSignUp() {
-        let askSheetController: UIAlertController = UIAlertController(title: "Welcome to Fresh!", message: "Create a Fresh account or log into an existing one to connect with farmers around the world.", preferredStyle: .Alert)
-        let signupAction: UIAlertAction = UIAlertAction(title: "Sign up", style: .Default) { action -> Void in
-            self.signUp()
-        }
-        askSheetController.addAction(signupAction)
-        
-        let loginAction: UIAlertAction = UIAlertAction(title: "Log in", style: .Default) { action -> Void in
-            self.signIn()
-        }
-        askSheetController.addAction(loginAction)
-        
-        let cancelAction: UIAlertAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
-        askSheetController.addAction(cancelAction)
-        
-        presentViewController(askSheetController, animated: true, completion: nil)
-    }
-    
-    var userEmail = ""
-    var userPassword = ""
-    func signUp() {
-        var emailTextField: UITextField?
-        var passwordTextField: UITextField?
-        
-        let signupSheetController: UIAlertController = UIAlertController(title: "Sign up to Fresh", message: "Create an account to connect with farmers around the world and fill your fridge with healthy food.", preferredStyle: .Alert)
-        
-        let signupAction: UIAlertAction = UIAlertAction(title: "Sign up", style: .Default) { action -> Void in
-            self.userEmail = emailTextField!.text!
-            self.userPassword = passwordTextField!.text!
-            print(self.userEmail)
-            print(self.userPassword)
-            
-            // Create the user
-            let user = PFUser()
-            self.userEmail = self.userEmail.lowercaseString
-            user.email = self.userEmail
-            user.username = self.userEmail
-            user.password = self.userPassword
-            
-            user.signUpInBackgroundWithBlock {
-                (succeeded: Bool, error: NSError?) -> Void in
-                if error == nil {
-                    print("Success!")
-                } else {
-                    print("Signing up failed.")
-                }
-            }
-        }
-        signupSheetController.addAction(signupAction)
-        let cancelAction: UIAlertAction = UIAlertAction(title: "Cancel", style: .Cancel) { action -> Void in }
-        signupSheetController.addAction(cancelAction)
-        
-        signupSheetController.addTextFieldWithConfigurationHandler({(textField: UITextField!) in
-            textField.textColor = UIColor.blackColor()
-            textField.placeholder = "Email"
-            textField.secureTextEntry = false
-            emailTextField = textField
-        })
-        
-        signupSheetController.addTextFieldWithConfigurationHandler({(textField: UITextField!) in
-            textField.textColor = UIColor.blackColor()
-            textField.placeholder = "Password"
-            textField.secureTextEntry = true
-            passwordTextField = textField
-        })
-        
-        presentViewController(signupSheetController, animated: true, completion: nil)
-    }
-    
-    func signIn() {
-        var emailTextField: UITextField?
-        var passwordTextField: UITextField?
-        
-        let signupSheetController: UIAlertController = UIAlertController(title: "Sign in to Fresh", message: "Log into your Fresh account to see what farmers are selling.", preferredStyle: .Alert)
-        
-        let signupAction: UIAlertAction = UIAlertAction(title: "Sign in", style: .Default) { action -> Void in
-            self.userEmail = emailTextField!.text!
-            self.userPassword = passwordTextField!.text!
-            print(self.userEmail)
-            print(self.userPassword)
-        }
-        signupSheetController.addAction(signupAction)
-        let cancelAction: UIAlertAction = UIAlertAction(title: "Cancel", style: .Cancel) { action -> Void in }
-        signupSheetController.addAction(cancelAction)
-        
-        signupSheetController.addTextFieldWithConfigurationHandler({(textField: UITextField!) in
-            textField.textColor = UIColor.blackColor()
-            textField.placeholder = "Email"
-            textField.secureTextEntry = false
-            emailTextField = textField
-        })
-        
-        signupSheetController.addTextFieldWithConfigurationHandler({(textField: UITextField!) in
-            textField.textColor = UIColor.blackColor()
-            textField.placeholder = "Password"
-            textField.secureTextEntry = true
-            passwordTextField = textField
-        })
-        
-        let user = PFUser()
-        user.email = userEmail
-        userEmail = userEmail.lowercaseString // ensure the email is lowercase
-        user.username = userEmail
-        user.password = userPassword
-        let userLogin = PFUser.currentUser()
-        
-        if (PFUser.currentUser() == nil) {
-            presentViewController(signupSheetController, animated: true, completion: nil)
-        } else {
-            print("User successfully authenticated!")
         }
     }
     
--- Fresh/Fresh/TableViewConroller.swift
+++
@@ -1,9 +0,0 @@
-//
-//  TableViewConroller.swift
-//  Fresh
-//
-//  Created by Owner on 10/10/15.
-//  Copyright © 2015 Chimica. All rights reserved.
-//
-
-import Foundation
